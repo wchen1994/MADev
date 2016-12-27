d7aa95e7bf37656c8c2c16c2aa686d528c4e7333
wchen1994 <weigan.chen@outlook.com>
2016-12-27 13:48:48 +1100

    game.cc add Player class and key handle require game class to handle player

--- bin/game.cc
+++ bin/game.cc
@@ -4,22 +4,50 @@ A touhou like game
 
 #include<SFML/Audio.hpp>
 #include<SFML/Graphics.hpp>
+#include<iostream>
+
+class Player{
+private:
+	sf::RenderWindow *wnd;
+	sf::CircleShape Sprite;
+public:
+	Player(sf::RenderWindow *wnd) :
+	Sprite(50)	
+	{
+		this->wnd = wnd;
+	}
+	void Update(){
+		wnd->draw(Sprite);
+	}
+};
+
+void handleKeyPressed(sf::Event::KeyEvent key){
+	sf::Keyboard::Key keycode = key.code;
+	std::cout << keycode << std::endl;
+}
 
 int main(){
 	sf::RenderWindow wnd(sf::VideoMode(800,600), "GAME");
 	wnd.setFramerateLimit(60);
 
-	sf::CircleShape ball(50);
-	ball.setFillColor(sf::Color(100,100,255));
+	Player player(&wnd);
 
 	while(wnd.isOpen()){
 		sf::Event event;
 		while(wnd.pollEvent(event)){
-			if(event.type == sf::Event::Closed)
-				wnd.close();
+			switch (event.type){
+				case sf::Event::KeyPressed:
+					handleKeyPressed(event.key);
+					break;
+				case sf::Event::Closed:
+					wnd.close();
+					break;
+				default:
+					break;
+			}
 		}
 		wnd.clear();
-		wnd.draw(ball);
+		player.Update();
 		wnd.display();
 	}
 
