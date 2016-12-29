0ac6f4515d92f48242184c8e68ef634315351011
wchen1994 <weigan.chen@outlook.com>
2016-12-29 13:45:50 +1100

    game.cc Fix player move as isotropy speed

--- bin/game.cc
+++ bin/game.cc
@@ -5,14 +5,18 @@ A touhou like game
 #include<SFML/Audio.hpp>
 #include<SFML/Graphics.hpp>
 #include<iostream>
+#include<cmath>
 
 class Player{
 private:
 	sf::RenderWindow *wnd;
 	sf::CircleShape sprite;
 	float x, y;
+	float dx, dy;
 	float originX, originY;
 	float radius;
+	float speed;
+	bool up, down, left, right;
 public:
 	Player(sf::RenderWindow *wnd) :
 		sprite()
@@ -21,6 +25,8 @@ public:
 		radius = 50;
 		x = 400;
 		y = 300;
+		dx = dy = 0;
+		speed = 10;
 		originX = originY = radius;
 		sprite.setRadius(radius);
 		sprite.setOrigin(originX, originY);
@@ -31,10 +37,41 @@ public:
 		wnd->draw(sprite);
 	}
 
-	void Move(float dx, float dy){
-		x += dx;
-		y += dy;
+	void Update(){
+		dx = dy = 0;
+		if (up)
+			dy -= 100;
+		if (down)
+			dy += 100;
+		if (left)
+			dx -= 100;
+		if (right)
+			dx += 100;
+		float sqlen =dx * dx + dy * dy;
+		
+		if (sqlen != 0){
+			sqlen = sqrt(sqlen);
+			x += dx/sqlen * speed;
+			y += dy/sqlen * speed;
+		}
+		std::cout << x << ' ' << y << std::endl;
 		sprite.setPosition(x, y);
+	}
+
+	void SetUp(bool status){
+		up = status;
+	}
+
+	void SetDown(bool status){
+		down = status;
+	}
+
+	void SetLeft(bool status){
+		left = status;
+	}
+
+	void SetRight(bool status){
+		right = status;
 	}
 };
 
@@ -51,20 +88,41 @@ public:
 		wnd.setFramerateLimit(60);
 	}
 
+	// Trigge as the key just be pressed
 	void handleKeyPressed(sf::Event::KeyEvent key){
 		sf::Keyboard::Key keycode = key.code;
 		switch (keycode){
 			case sf::Keyboard::W:
-				player.Move(0,-30);
+				player.SetUp(true);
 				break;
 			case sf::Keyboard::S:
-				player.Move(0,30);
+				player.SetDown(true);
 				break;
 			case sf::Keyboard::A:
-				player.Move(-30,0);
+				player.SetLeft(true);
 				break;
 			case sf::Keyboard::D:
-				player.Move(30,0);
+				player.SetRight(true);
+				break;
+			default:
+				break;
+		}
+	}
+
+	void handleKeyReleased(sf::Event::KeyEvent key){
+		sf::Keyboard::Key keycode = key.code;
+		switch (keycode){
+			case sf::Keyboard::W:
+				player.SetUp(false);
+				break;
+			case sf::Keyboard::S:
+				player.SetDown(false);
+				break;
+			case sf::Keyboard::A:
+				player.SetLeft(false);
+				break;
+			case sf::Keyboard::D:
+				player.SetRight(false);
 				break;
 			default:
 				break;
@@ -83,6 +141,9 @@ public:
 				case sf::Event::KeyPressed:
 					handleKeyPressed(event.key);
 					break;
+				case sf::Event::KeyReleased:
+					handleKeyReleased(event.key);
+					break;
 				case sf::Event::Closed:
 					wnd.close();
 					break;
@@ -90,6 +151,8 @@ public:
 					break;
 			}
 		}
+		player.Update();
+
 		wnd.clear();
 		player.Draw();
 		wnd.display();
